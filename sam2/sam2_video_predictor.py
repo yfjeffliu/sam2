86646d93151e1d8bb6f0590f116f3554c6dbcb67
yfjeffliu <yf.jeff.liu@gmail.com>
2025-01-13 14:55:59 +0800

    add device setting

--- sam2/sam2_video_predictor.py
+++ sam2/sam2_video_predictor.py
@@ -725,21 +725,21 @@ class SAM2VideoPredictor(SAM2Base):
                 cache_data = pickle.loads(cache_data)
                 device = inference_state["device"]
                 image = cache_data["image"]
-                image = torch.tensor(image).to(device).float()
+                image = torch.tensor(image).to(device)
                 backbone_out = cache_data["image_embedding"]
                 inference_state["cached_features"] = {frame_idx: (image, backbone_out)}
                 # print('get preload image from redis')
             else:
                 backbone_out = None
-        if backbone_out is None:
-            # Cache miss -- we will run inference on a single image
-            # print('cache miss')
-            device = inference_state["device"]
-            image = inference_state["images"][frame_idx].to(device).float().unsqueeze(0)
-            backbone_out = self.forward_image(image)
-            # Cache the most recent frame's feature (for repeated interactions with
-            # a frame; we can use an LRU cache for more frames in the future).
-            inference_state["cached_features"] = {frame_idx: (image, backbone_out)}
+        # if backbone_out is None:
+        #     # Cache miss -- we will run inference on a single image
+        #     # print('cache miss')
+        #     device = inference_state["device"]
+        #     image = inference_state["images"][frame_idx].to(device).float().unsqueeze(0)
+        #     backbone_out = self.forward_image(image)
+        #     # Cache the most recent frame's feature (for repeated interactions with
+        #     # a frame; we can use an LRU cache for more frames in the future).
+        #     inference_state["cached_features"] = {frame_idx: (image, backbone_out)}
 
         # expand the features to have the same dimension as the number of objects
         expanded_image = image.expand(batch_size, -1, -1, -1)
